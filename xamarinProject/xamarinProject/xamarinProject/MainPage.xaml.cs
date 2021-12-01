6a6a61a4c16123a7245bfdb559093d60a4745565
Rewax <daniel.lorenzen@hotmail.com>
2021-12-01 16:43:33 +0100

    Tidligere fejl fikset, mangler at få koordinaterne frem på kortet

--- xamarinProject/xamarinProject/xamarinProject/MainPage.xaml.cs
+++ xamarinProject/xamarinProject/xamarinProject/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace xamarinProject
 {
--- xamarinProject/xamarinProject/xamarinProject/maps.xaml.cs
+++ xamarinProject/xamarinProject/xamarinProject/maps.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace xamarinProject
@@ -12,14 +13,55 @@ namespace xamarinProject
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class maps : ContentPage
     {
+
+
         public maps()
         {
             InitializeComponent();
+            UpdateMap();
         }
 
         private async void NavigateButton_OnClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new MainPage());
+        }
+
+        List<Place> placesList = new List<Place>();
+
+        private void UpdateMap()
+        {
+            try
+            {
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(maps)).Assembly;
+                Stream stream = assembly.GetManifestResourceStream("xamarinProject.Places.json");
+                string text = string.Empty;
+                using (var reader = new StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                var resultObject = JsonConvert.DeserializeObject<Places>(text);
+
+                foreach (var place in resultObject.features)
+                {
+                    placesList.Add(new Place
+                    {
+                        PlaceName = place.properties.Name,
+                        Location = place.geometry.type,
+                        Position = new Position(place.geometry.coordinates[0], place.geometry.coordinates[1])
+                    });
+                }
+
+                MyMap.ItemsSource = placesList;
+                //MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(47.6370891183, -122.123736172), Distance.FromKilometers(100)));
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+
         }
     }
 }
