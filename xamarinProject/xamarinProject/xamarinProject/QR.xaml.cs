b8f924696fe1f159a49f869cd5d397f18ffe2aab
SoSoCho <70580037+SoSoCho@users.noreply.github.com>
2021-12-13 09:21:49 +0100

    update ver. 2

--- xamarinProject/xamarinProject/xamarinProject/QR.xaml.cs
+++ xamarinProject/xamarinProject/xamarinProject/QR.xaml.cs
@@ -1,24 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace xamarinProject
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class QR : ContentPage
+    public partial class QRPage : ContentPage
     {
-        public QR()
+        public QRPage()
         {
             InitializeComponent();
+
         }
-        public async void QR_Clicked(object sender, EventArgs e)
+
+        private void InitializeComponent()
         {
-            await Navigation.PushAsync(new QR());
+        //    throw new NotImplementedException();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync();
+                    await DisplayAlert("Value QRCode", "" + result.Text, "OK");
+
+                });
+
+            };
         }
     }
 }
