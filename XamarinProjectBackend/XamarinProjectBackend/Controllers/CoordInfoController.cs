1058408d162c2db739eb929ceec7ac8a168a880b
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2021-12-15 13:21:32 +0100

    edit done

--- XamarinProjectBackend/XamarinProjectBackend/Controllers/CoordInfoController.cs
+++ XamarinProjectBackend/XamarinProjectBackend/Controllers/CoordInfoController.cs
@@ -79,15 +79,15 @@ namespace XamarinProjectBackend.Controllers
 
             foreach (var place in resultObject.features)
                 {
-                    obj.name = place.properties.Name;
-                    obj.description = place.properties.description;
-                    obj.type = place.geometry.type;
-                    obj.lat = place.geometry.coordinates[0].ToString();
-                    obj.lon = place.geometry.coordinates[1].ToString();
+                    obj.Name = place.properties.Name;
+                    obj.Description = place.properties.description;
+                    obj.Type = place.geometry.type;
+                    obj.Lat = place.geometry.coordinates[0].ToString();
+                    obj.Lon = place.geometry.coordinates[1].ToString();
 
                 // identity_insert is set to off sql server
                 // SET IDENTITY_INSERT tableName ON
-                _db.Database.ExecuteSqlRaw($"INSERT INTO dbo.Coordinfo (name, description, type, lat, lon) values ('{place.properties.Name}', '{place.properties.description}', '{obj.type = place.geometry.type}', '{place.geometry.coordinates[0]}', '{place.geometry.coordinates[1]}');");
+                _db.Database.ExecuteSqlRaw($"INSERT INTO dbo.Coordinfo (name, description, type, lat, lon) values ('{place.properties.Name}', '{place.properties.description}', '{obj.Type = place.geometry.type}', '{place.geometry.coordinates[0]}', '{place.geometry.coordinates[1]}');");
                     _db.SaveChanges();
 
             }
--- XamarinProjectBackend/XamarinProjectBackend/Models/CoordInfo.cs
+++ XamarinProjectBackend/XamarinProjectBackend/Models/CoordInfo.cs
@@ -10,14 +10,13 @@ namespace XamarinProjectBackend.Models
     public class CoordInfo
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int id { get; set; }
-        public string name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
 
-        public string description { get; set; }
-        public string type { get; set; }
-        public string lat { get; set; }
-        public string lon { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
 
         //public CoordInfo(string name, string description, string type, double lat, double lon)
         //{
